20de1b24e6de81977b78e27b2f3a189ae51b2173
Nitin Jilla <nitinjilla9@gmail.com>
2023-02-05 16:35:55 -0500

    added node disk size

--- sre/tf-scripts/gke.tf
+++ sre/tf-scripts/gke.tf
@@ -52,6 +52,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     # preemptible  = true
     machine_type = "n1-standard-1"
+    disk_size_gb = 25
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
